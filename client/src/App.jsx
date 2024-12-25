f8a7851aafbff240dd370daa35b78f6f805de327
Dhruv <dhruvchaudhary1611@gmail.com>
2024-12-25 20:39:57 +0530

    Initialized Frontend

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <>
+      <Button variant="outline">Button</Button>
+      <h1 className="text-blue-500">Hello</h1>
+    </>
+  );
+}
+
+export default App;
